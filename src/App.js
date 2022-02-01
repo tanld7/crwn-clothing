041f78c5d048391e8d4c4530f39bb8ef38c99c91
tanld7 <tanld.dev@gmail.com>
2022-02-01 01:36:38 -0800

    intergrated firebase into application, created sign in page with sign in component and sign in with google

--- src/App.js
+++ src/App.js
@@ -6,20 +6,49 @@ import './App.css';
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shoppage/shoppage.component";
 import Header from "./components/header/header.component";
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
+
+import { auth } from './firebase/firebase.utils'
+import userEvent from "@testing-library/user-event";
 
 
-function App() {
-  return (
-      <div>
 
-          <BrowserRouter>
-              <Header />
-              <Route exact path='/' component={HomePage} />
-              <Route path='/shop' component={ShopPage} />
-          </BrowserRouter>
-      </div>
+class App extends React.Component {
 
-  );
+    constructor() {
+        super();
+        this.state = {
+            currentUser: null,
+        }
+    }
+
+    unsubscribeFromAuth = null;
+
+    componentDidMount() {
+        this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+            this.setState({currentUser: user});
+
+            // console.log(user);
+        })
+    }
+
+    componentWillUnmount() {
+        this.unsubscribeFromAuth();
+    }
+
+    render() {
+        return (
+            <div>
+
+                <BrowserRouter>
+                    <Header currentUser={this.state.currentUser}/>
+                    <Route exact path='/' component={HomePage} />
+                    <Route path='/shop' component={ShopPage} />
+                    <Route path='/signin' component={SignInAndSignUpPage}/>
+                </BrowserRouter>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,27 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const config = {
+    apiKey: "AIzaSyCu3EWINejeqggXwd6nmzlNmF3vXUrTFSE",
+    authDomain: "crwn-db-e6b63.firebaseapp.com",
+    projectId: "crwn-db-e6b63",
+    storageBucket: "crwn-db-e6b63.appspot.com",
+    messagingSenderId: "952073022037",
+    appId: "1:952073022037:web:3e27038a5be94a3b5b02dc",
+    measurementId: "G-PQF9N0WJ9J"
+};
+
+firebase.initializeApp(config)
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+/** Setup Google Auth Utility */
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
