ebf10283f37b3975b00861012cbcab6c81c23bd3
tanld7 <tanld.dev@gmail.com>
2022-02-06 01:38:31 -0800

    Add some minor changes

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -1,65 +1,64 @@
 import React from 'react';
 
-import FormInput from "../form-input/form-input.component";
-import CustomButton from "../custom-button/custom-button.component";
+import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
 
-import {auth, createUserProfileDocument} from "../../firebase/firebase.utils";
+import { auth, createUserProfileDocument } from '../../firebase/firebase.utils';
 
-import './sign-up.styles.scss'
+import './sign-up.styles.scss';
 
 class SignUp extends React.Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
 
         this.state = {
             displayName: '',
             email: '',
             password: '',
-            confirmPassword: '',
-        }
+            confirmPassword: ''
+        };
     }
 
-    handleSubmit = async (event) => {
+    handleSubmit = async event => {
         event.preventDefault();
 
         const { displayName, email, password, confirmPassword } = this.state;
 
         if (password !== confirmPassword) {
-            alert("Passwords don't match");
+            alert("passwords don't match");
             return;
         }
 
         try {
-            const { user } = await auth.createUserWithEmailAndPassword(email, password);
+            const { user } = await auth.createUserWithEmailAndPassword(
+                email,
+                password
+            );
 
-            await createUserProfileDocument(user, {displayName});
+            await createUserProfileDocument(user, { displayName });
 
             this.setState({
                 displayName: '',
                 email: '',
                 password: '',
-                confirmPassword: '',
+                confirmPassword: ''
             });
-
-        } catch (e) {
-            console.error(e);
+        } catch (error) {
+            console.error(error);
         }
-    }
+    };
 
-    handleChange = (event) => {
+    handleChange = event => {
         const { name, value } = event.target;
-        this.setState({
-            [name]: value
-        });
-    }
 
+        this.setState({ [name]: value });
+    };
 
     render() {
         const { displayName, email, password, confirmPassword } = this.state;
-
         return (
             <div className='sign-up'>
-                <h2 className='title'>I do not have an account</h2>
+                <h2 className='title'>I do not have a account</h2>
                 <span>Sign up with your email and password</span>
                 <form className='sign-up-form' onSubmit={this.handleSubmit}>
                     <FormInput
@@ -94,10 +93,10 @@ class SignUp extends React.Component {
                         label='Confirm Password'
                         required
                     />
-                    <CustomButton type='submit' on >SIGN UP</CustomButton>
+                    <CustomButton type='submit'>SIGN UP</CustomButton>
                 </form>
             </div>
-        )
+        );
     }
 }
 
