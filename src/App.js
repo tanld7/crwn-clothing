9532e2431e3a5e34787f9d66820854343578b978
tanld7 <tanld.dev@gmail.com>
2022-02-10 03:26:43 -0800

    Using reselect library, create user selector and use it in header component; refactor cart-icon, cart-dropdown to use reselect

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, BrowserRouter, Redirect } from 'react-router-dom';
 import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
 
 import './App.css';
 
@@ -12,6 +13,7 @@ import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up
 import { auth, createUserProfileDocument } from './firebase/firebase.utils'
 
 import { setCurrentUser } from "./redux/user/user.actions";
+import {selectCurrentUser} from "./redux/user/user.selectors";
 
 
 class App extends React.Component {
@@ -60,8 +62,8 @@ class App extends React.Component {
     }
 }
 
-const mapStateToProps = ({ user }) => ({
-    currentUser: user.currentUser
+const mapStateToProps = createStructuredSelector({
+    currentUser: selectCurrentUser,
 })
 
 const mapDispatchToProps = dispatch => ({
