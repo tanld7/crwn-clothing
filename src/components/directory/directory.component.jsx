77376ca3b859f4a80fe85bb37e6ce2f4167a6e31
tanld7 <tanld.dev@gmail.com>
2022-02-13 00:24:02 -0800

    moved sections directory to directoryReducer, also added directory selector

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,70 +1,35 @@
 import React, { Component } from 'react';
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
 
-import './directory.styles.scss';
+import { selectDirectorySections} from "../../redux/directory/directory.selectors";
 
 import MenuItem from "../menu-item/menu-item.component";
 
-class Directory extends React.Component {
-    constructor() {
-        super();
+import './directory.styles.scss';
 
-        this.state = {
-            sections: [
-                {
-                    title: 'hats',
-                    imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-                    id: 1,
-                    linkUrl: 'shoppage/hats'
-                },
-                {
-                    title: 'jackets',
-                    imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-                    id: 2,
-                    linkUrl: 'shoppage/jackets'
-                },
-                {
-                    title: 'sneakers',
-                    imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-                    id: 3,
-                    linkUrl: 'shoppage/sneakers'
-                },
-                {
-                    title: 'womens',
-                    imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-                    size: 'large',
-                    id: 4,
-                    linkUrl: 'shoppage/womens'
-                },
-                {
-                    title: 'mens',
-                    imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-                    size: 'large',
-                    id: 5,
-                    linkUrl: 'shoppage/mens'
-                }
-            ],
-        }
-    }
 
-    render() {
-        return (
-            <div className='directory-menu'>
-                {this.state.sections.map(({ title, imageUrl, id, size, linkUrl }) => {
-                    return (
-                        <MenuItem
-                            key={id}
-                            title={title}
-                            imageUrl={imageUrl}
-                            size={size}
-                            linkUrl={linkUrl}
-                        />
-                    )
-                })}
-            </div>
-        )
-    }
-
+const Directory = ({ sections }) => {
+    return (
+        <div className='directory-menu'>
+            {sections.map(({title, imageUrl, id, size, linkUrl}) => {
+                return (
+                    <MenuItem
+                        key={id}
+                        title={title}
+                        imageUrl={imageUrl}
+                        size={size}
+                        linkUrl={linkUrl}
+                    />
+                )
+            })}
+        </div>
+    )
 }
 
-export default Directory;
+const mapStateToProps = createStructuredSelector({
+    sections: selectDirectorySections,
+})
+
+export default connect(mapStateToProps)(Directory);
 
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -4,16 +4,19 @@ import storage from 'redux-persist/lib/storage';
 
 import userReducer from "./user/user.reducer";
 import cartReducer from "./cart/cart.reducer";
+import directoryReducer from "./directory/directory.reducer";
+
+
+const rootReducer = combineReducers({
+    user: userReducer,
+    cart: cartReducer,
+    directory: directoryReducer,
+});
 
 const persistConfig = {
     key: 'root',
     storage,
     whitelist: ['cart', ]
 }
-
-const rootReducer = combineReducers({
-    user: userReducer,
-    cart: cartReducer,
-});
 
 export default persistReducer(persistConfig, rootReducer)
