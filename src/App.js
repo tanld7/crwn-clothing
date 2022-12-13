a9d421b1eaa34d570cfd7072e15a5caae4d30daf
Tan Le <tanld.dev@gmail.com>
2022-12-13 20:59:08 +0700

    Updated the app to navigate to home page if user is logged in.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import {connect} from "react-redux";
 
 import './App.css';
@@ -12,7 +12,11 @@ import { auth, createUserProfileDocument } from './firebase/firebase.utils'
 
 import {setCurrentUser} from "./redux/user/user.actions";
 
+
+
 class App extends React.Component {
+
+
     unsubscribeFromAuth = null;
 
     componentDidMount() {
@@ -50,15 +54,26 @@ class App extends React.Component {
                 <Routes>
                     <Route exact path='/' element={<HomePage/>} />
                     <Route path='/shop' element={<ShopPage/>} />
-                    <Route path='/signin' element={<SignInAndSignUpPage/>} />
+                    <Route
+                        exact
+                        path='/signin'
+                        element={this.props.currentUser
+                            ? (<Navigate to='/' />)
+                            : (<SignInAndSignUpPage/>)
+                        }
+                    />
               </Routes>
             </div>
       );
     }
 }
 
+const mapStateToProps = ({user}) => ({
+    currentUser: user.currentUser,
+})
+
 const mapDispatchToProps = (dispatch) => ({
     setCurrentUser: user => dispatch(setCurrentUser(user))
 });
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
