82b979dd1ac40300e3d2bfd1c2d92f3c6fda9477
Tan Le <tanld.dev@gmail.com>
2022-12-22 16:32:41 +0700

    Implemented persisting cart data functionality with redux-persist (using localStorage).

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 
-import store from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "./redux/store";
 
 import './index.css';
 import App from './App';
@@ -12,7 +13,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <Provider store={store}>
         <BrowserRouter>
-            <App />
+            <PersistGate persistor={persistor}>
+                <App />
+            </PersistGate>
         </BrowserRouter>
     </Provider>
 );
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,19 @@
 import {combineReducers} from "redux";
+import {persistReducer} from "redux-persist";
+import storage from 'redux-persist/lib/storage'; //tell redux-persist that I want to use the localStorage as default storage.
 
 import userReducer from "./user/user.reducer";
 import cartReducer from "./cart/cart.reducer";
 
-export default combineReducers({
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['cart']  // only cart reducer will be persisted
+};
+
+const rootReducer = combineReducers({
     user: userReducer,
     cart: cartReducer,
-})
+});
+
+export default persistReducer(persistConfig, rootReducer);
