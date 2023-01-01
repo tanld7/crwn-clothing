82bdb732a543d809e3142eed6cb40651a3de0d6e
Tan Le <tanld.dev@gmail.com>
2023-01-01 22:23:32 +0700

    Refactor Header and Custom-button components to use styled-components.

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import './custom-button.styles.scss'
 
-const CustomButton = ({children, isGoogleSignIn, inverted, ...otherProps}) => {
+import { CustomButtonContainer } from './custom-button.styles'
+
+const CustomButton = ({children, ...props}) => {
     return (
-        <button className={`${inverted ? 'inverted' : ''} ${ isGoogleSignIn ? 'google-sign-in' : '' } custom-button`} {...otherProps}>
+        <CustomButtonContainer {...props}>
             {children}
-        </button>
+        </CustomButtonContainer>
     );
 }
 
